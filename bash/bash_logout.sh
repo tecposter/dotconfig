93414baaf407b910e0f816342c9774e183464559
zjh <zhanjh@126.com>
2017-08-19 12:30:07 +0800

    Modify bash

--- bash/bash_logout.sh
+++ bash/bash_logout.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ "$SHLVL" = 1 ]; then
+    [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+fi
+
 # clean terminal
 reset
--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -252,3 +252,14 @@ PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 
 # sudo apt install autojump
 [ -f /usr/share/autojump/autojump.sh ] && . /usr/share/autojump/autojump.sh
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
