105eb84a327d07fd512a208b416d65c3573ebf08
jason <changyuanjian@gmail.com>
2017-08-22 10:34:25 +0800

    nvm

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -252,3 +252,7 @@ PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 
 # sudo apt install autojump
 [ -f /usr/share/autojump/autojump.sh ] && . /usr/share/autojump/autojump.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
